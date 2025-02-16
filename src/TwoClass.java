01af308f14aa0cda68c7e525874a62639457b148
홍길동 <seongjin3378@naver.com>
2025-02-16 21:26:26 +0900

    first commit

---
+++ src/TwoClass.java
@@ -0,0 +1,8 @@
+public class TwoClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("git Branch 테스트 ㄷㅈㄹㄷㅈㄹㅈㄹㅈ");
+    }
+}
